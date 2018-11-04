4f5ca7391563aceda9b34acb887f4fd69d2d9649
Eugen Cazacu <green182day@hotmail.com>
2018-11-04 23:48:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { context } from './store/MyContext.js';
+import Task from './components/Task.js';
+import EditTask from './components/EditTask.js';
 
 class App extends Component {
+
+  handleDrop = (e, newStatus) => {
+    const id = e.dataTransfer.getData("id");
+    this.context.dispatch({ type: "CHANGE_STATUS", id, newStatus });
+    e.target.classList.remove("dragover");
+  }
+  handleDragOver = (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains("taskcontainer")) {
+      e.target.classList.add("dragover");
+    }
+  }
+  handleDragExit = (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains("taskcontainer")) {
+      e.target.classList.remove("dragover");
+    }
+  }
+  handleDragLeave = (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains("taskcontainer")) {
+      e.target.classList.remove("dragover");
+    }
+  }
+
   render() {
+    let todolist = <div></div>;
+    let doinglist = <div></div>;
+    let donelist = <div></div>;
+    if (this.context.tasks) {
+      todolist = this.context.tasks
+        .filter(t => t.status === "todo")
+        .map(t => <Task key={t.id} id={t.id} title={t.title} content={t.content} status={t.status} color={t.color} active={t.active} />);
+
+      doinglist = this.context.tasks
+        .filter(t => t.status === "doing")
+        .map(t => <Task key={t.id} id={t.id} title={t.title} content={t.content} status={t.status} color={t.color} active={t.active} />);
+
+      donelist = this.context.tasks
+        .filter(t => t.status === "done")
+        .map(t => <Task key={t.id} id={t.id} title={t.title} content={t.content} status={t.status} color={t.color} active={t.active} />);
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container app-container">
+          <div className="row navbar-header">
+            <div className="col-3 dashboard-header text-center">
+            </div>
+            <div className="col-3 todo-header text-center">
+              Todo
+            </div>
+            <div className="col-3 doing-header text-center">
+              Doing
+            </div>
+            <div className="col-3 done-header text-center">
+              Done
+            </div>
+          </div>
+          <div className="row task-row">
+            <div className="col-3 dashboard">
+              <EditTask />
+            </div>
+            <div className="col-3 todo taskcontainer" droppable="true" onDrop={(e) => { this.handleDrop(e, "todo") }} onDragOver={this.handleDragOver} onDragExit={this.handleDragExit} onDragLeave={this.handleDragLeave}>
+              {todolist}
+            </div>
+            <div className="col-3 doing taskcontainer" droppable="true" onDrop={(e) => { this.handleDrop(e, "doing") }} onDragOver={this.handleDragOver} onDragExit={this.handleDragExit} onDragLeave={this.handleDragLeave}>
+              {doinglist}
+            </div>
+            <div className="col-3 done taskcontainer" droppable="true" onDrop={(e) => { this.handleDrop(e, "done") }} onDragOver={this.handleDragOver} onDragExit={this.handleDragExit} onDragLeave={this.handleDragLeave}>
+              {donelist}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+App.contextType = context;
+export default App
