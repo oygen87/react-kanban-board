de247ab9d79dae4a69e39c1b0232d5612c82d1ef
Eugen Cazacu <green182day@hotmail.com>
2018-11-10 12:49:08 +0100

    add build

---
+++ build/precache-manifest.8c363dcd5ba680b56683bcb7e152e43e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "349ba48be9fcd71b86e5",
+    "url": "/static/js/main.349ba48b.chunk.js"
+  },
+  {
+    "revision": "bffbd15f3b77b7e5b0fd",
+    "url": "/static/js/1.bffbd15f.chunk.js"
+  },
+  {
+    "revision": "349ba48be9fcd71b86e5",
+    "url": "/static/css/main.f5e6e7be.chunk.css"
+  },
+  {
+    "revision": "0bcbaca2547c3083275b43fa2d6616da",
+    "url": "/index.html"
+  }
+];
